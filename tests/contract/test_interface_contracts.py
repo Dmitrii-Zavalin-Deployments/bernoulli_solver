a633170329fcff917d6b8a945c20e1ac5ab12d60
Dmitrii Zavalin <zavalin.dmitry@gmail.com>
2026-05-31 07:59:59 -0600

    Implementation of the python tests

--- tests/contract/test_interface_contracts.py
+++ tests/contract/test_interface_contracts.py
@@ -1,4 +1,6 @@
 import pytest
+from tests.dummies.dummy_bernoulli_state import BernoulliStateDummy
+
 from src.interfaces.step_interfaces.step_s0_filled_unfilled_classifier_interface import FilledUnfilledClassifierInterface
 from src.interfaces.step_interfaces.step_s1_exactly_one_missing_interface import StepS1ExactlyOneMissingInterface
 from src.interfaces.step_interfaces.step_s2_construct_partial_state_interface import StepS2ConstructPartialStateInterface
@@ -6,32 +8,38 @@ from src.interfaces.step_interfaces.step_s3_solve_missing_variable_interface imp
 from src.interfaces.step_interfaces.step_s4_compute_energy_residual_interface import StepS4ComputeEnergyResidualInterface
 from src.interfaces.step_interfaces.step_s5_compute_min_max_constraints_interface import StepS5ComputeMinMaxConstraintsInterface
 
-def test_s0_interface_contract():
+@pytest.fixture
+def dummy_state():
+    return BernoulliStateDummy()
+
+def test_s0_interface_contract(dummy_state):
     interface = FilledUnfilledClassifierInterface()
     with pytest.raises(NotImplementedError):
-        interface.classify_filled_and_unfilled(None)
+        interface.classify_filled_and_unfilled(dummy_state)
 
-def test_s1_interface_contract():
+def test_s1_interface_contract(dummy_state):
     interface = StepS1ExactlyOneMissingInterface()
     with pytest.raises(NotImplementedError):
-        interface.enforce_exactly_one_missing({})
+        # We pass the dummy directly. This ensures the interface 
+        # is capable of accepting the expected object type.
+        interface.enforce_exactly_one_missing(dummy_state)
 
-def test_s2_interface_contract():
+def test_s2_interface_contract(dummy_state):
     interface = StepS2ConstructPartialStateInterface()
     with pytest.raises(NotImplementedError):
-        interface.construct_partial_state({}, "", None)
+        interface.construct_partial_state(dummy_state, "p1", 1.0)
 
-def test_s3_interface_contract():
+def test_s3_interface_contract(dummy_state):
     interface = StepS3SolveMissingVariableInterface()
     with pytest.raises(NotImplementedError):
-        interface.solve_missing_variable(None, None)
+        interface.solve_missing_variable(dummy_state, None) # Assuming None is config
 
-def test_s4_interface_contract():
+def test_s4_interface_contract(dummy_state):
     interface = StepS4ComputeEnergyResidualInterface()
     with pytest.raises(NotImplementedError):
-        interface.compute_energy_and_residual(None, None)
+        interface.compute_energy_and_residual(dummy_state, None)
 
-def test_s5_interface_contract():
+def test_s5_interface_contract(dummy_state):
     interface = StepS5ComputeMinMaxConstraintsInterface()
     with pytest.raises(NotImplementedError):
-        interface.compute_min_max_constraints(None, None)
+        interface.compute_min_max_constraints(dummy_state, None)
