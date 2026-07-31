0a604df2d6715f3ad946d6d83cc31a1add6cf17e
Dmitrii Zavalin <zavalin.dmitry@gmail.com>
2026-07-31 12:23:56 -0600

    Phase 2: Core Sprint

---
+++ src/utils/__init__.py
@@ -0,0 +1,13 @@
+from src.utils.renderer import (
+    render_physical_boundary_map,
+    render_spatial_location_map,
+    render_step_snapshot,
+    set_debug_mode,
+)
+
+__all__ = [
+    "render_physical_boundary_map",
+    "render_spatial_location_map",
+    "render_step_snapshot",
+    "set_debug_mode",
+]
